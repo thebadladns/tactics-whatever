c81699f2fd91ef01d78213e68341e7f853ed9aea
Rafa de la Hoz <rhss@gmv.com>
2019-09-12 11:07:39 +0200

    Removed the declaration of implementing a no longer implementable interface in TiledPropertySet

--- source/utils/tiled/TiledPropertySet.hx
+++ source/utils/tiled/TiledPropertySet.hx
@@ -7,7 +7,7 @@ import haxe.xml.Fast;
  * (original by Matt Tuttle based on Thomas Jahn's. Haxe port by Adrien Fischer)
  * This content is released under the MIT License.
  */
-class TiledPropertySet implements Dynamic<String>
+class TiledPropertySet
 {
 	public function new()
 	{
