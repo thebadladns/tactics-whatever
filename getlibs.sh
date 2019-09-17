b7d9c0b1d3687c660805b2bd0966bde82dfee61e
Miguel Márquez <mmaltuna@gmail.com>
2019-09-17 16:10:40 +0200

    - Added scripts to set all libs to agreed versions - .bat needs to receive "y" input manually because I don't know a "yes" alternative in Windows (echo y | doesn't seem to work)

---
+++ getlibs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+yes | haxelib set actuate 1.8.9
+yes | haxelib set box2d 1.2.3
+yes | haxelib set flixel-addons 2.7.4
+yes | haxelib set flixel-demos 2.7.0
+yes | haxelib set flixel-templates 2.6.0
+yes | haxelib set flixel-tools 1.4.4
+yes | haxelib set flixel-ui 2.3.2
+yes | haxelib set flixel 4.6.0
+yes | haxelib set hscript 2.3.0
+yes | haxelib set hxcpp 4.0.8
+yes | haxelib set layout 1.2.1
+yes | haxelib set lime-samples 7.0.0
+yes | haxelib set lime 7.3.0
+yes | haxelib set openfl-samples 8.7.0
+yes | haxelib set openfl 8.9.0
+yes | haxelib set systools 1.1.0
